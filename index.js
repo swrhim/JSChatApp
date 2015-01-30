fd1f477b913caf6fd67f634772262b292997385e
swrhim <swrhim@gmail.com>
2015-01-31 08:47:06 +0900

    Uses self signed certificate for encryption of messages
    For dev purposes, we’re using a self signed certificate.
    Known issues:
    
    - requires the url to have “https” We need to configure this to auto
    route http requests as well

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var express = require('express')
-  , app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+  , app = express()
+  , fs = require('fs')
+  , options = {
+    key: fs.readFileSync('./cert/client.key'),
+    cert: fs.readFileSync('./cert/client.crt'),
+    requiestCert:true
+  };
+var https = require('https').Server(options, app);
+var io = require('socket.io')(https);
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 
 app.get('/', function(req, res){
   //res.send('<h1>TEST</h1>');
@@ -10,13 +19,26 @@ app.get('/', function(req, res){
 
 app.use(express.static(__dirname + '/css'));
 
+//redirect to occur even if the client does not specify the https
+app.use(function(req, res, next) {
+  if(!req.secure) {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+});
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
 
-http.listen(3000, function()
+https.listen(3000, function()
 {
   console.log('listening on *:3000');
 });
+
+
+
+
+
+
